1f2f4228fcea1b138ebd7eba7a16a581ac057760
Abraham Hernandez <abeherham@knights.ucf.edu>
2020-06-08 10:40:29 -0400

    Fixed Issues

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.use(gameRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build/"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const MongoURI = process.env.MongoURI;
