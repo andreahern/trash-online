fdf737e3cd53232ed8aa106f9da81e3851320de8
Abraham Hernandez <abeherham@knights.ucf.edu>
2020-06-08 11:24:53 -0400

    Fixed Issues

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ app.use(authRoutes);
 app.use(gameRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build/"));
+  app.use(express.static(path.join(__dirname, "client/build/")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "build"));
   });
 }
 
